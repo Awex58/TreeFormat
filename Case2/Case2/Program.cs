f5ca7aed1be4b8ba379d9dfff021e568ee7541a4
Awex58 <yunus.emre.yigit@hotmail.com>
2024-03-06 11:10:21 +0300

    Tree Format Console

---
+++ Case2/Case2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+again: 
+Console.Write("Uygun formatta giriş yapınız örn:'abccbdeeda':  ");
+string? word = Console.ReadLine();
+
+Stack<char> letterstack = new Stack<char>();
+int x = -1;
+
+if (word != null)
+foreach (char letter in word)
+{
+    if (letterstack.Count > 0 && letterstack.Peek() == letter)
+    {
+        letterstack.Pop();
+        x--;
+    }
+    else
+    {
+        if (letterstack.Count > 0)
+            Console.Write("-");
+
+        for (int i = 0; i < x; i++)
+            Console.Write("-");
+
+        Console.WriteLine(letter);
+        letterstack.Push(letter);
+        x++;
+    }
+}
+goto again;
